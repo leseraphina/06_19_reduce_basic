a0bee4076333235911af63f391650e22e0218537
leseraphina <leseraphina@gamil.com>
2023-06-20 12:55:08 +0900

    end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import store from './store02';
+import store from './store03';
 
 import Home from './component/Home';
 import Cart01 from './component/Cart01';
 import Cart02 from './component/Cart02';
 import Cart03 from './component/Cart03';
+import Cart04 from './component/Cart04';
 
 
 
@@ -20,6 +21,7 @@ function App() {
       <Route path="/cart01" element={<Cart01 />} />
       <Route path="/cart02" element={<Cart02 />} />
       <Route path="/cart03" element={<Cart03 />} />
+      <Route path="/cart04" element={<Cart04 />} />
     </Routes>
   </div>
   </BrowserRouter>
